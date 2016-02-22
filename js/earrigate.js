1d52beded8d2ae6906f00cfa1e657a0ce964bf88
kylienic <kyliegnic@gmail.com>
2016-02-22 09:30:58 -0500

    yup

--- js/earrigate.js
+++ js/earrigate.js
@@ -1,11 +1,22 @@
-$( ".form-signin" ).submit(function() {
-    $('.logo').animate({
-        bottom: '-500px'
+/* $( ".form-signin" ).submit(function() {
+    $("#img-drop").animate({
+        // bottom: '-500px'
+        top: auto,
+        bottom: '0px'
     }, 1000)
-    // $('.fadein').animate({
-    // 	opacity: '0.1'
-    // }, 2000)
 	$(".fadein").fadeIn(2000);
+    $('.water').delay(800).animate({
+        height: '100%'
+    }, 2000)
+});
+*/
+
+$( "#click-subscribe").click(function() {
+    $("#img-drop").animate({
+        // bottom: '-500px'
+        top: '1000px'
+    }, 2000)
+    $(".fadein").delay(400).fadeIn(2000);
     $('.water').delay(800).animate({
         height: '100%'
     }, 2000)
